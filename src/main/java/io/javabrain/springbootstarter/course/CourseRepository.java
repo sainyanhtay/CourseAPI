10ecbb815312eae0d23546529641ad76273253fb
sainyanhtay <sainyanhtay@gmail.com>
2019-03-12 15:13:47 +0630

    first commit

---
+++ src/main/java/io/javabrain/springbootstarter/course/CourseRepository.java
@@ -0,0 +1,9 @@
+package io.javabrain.springbootstarter.course;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, String>{
+
+	
+	
+}
